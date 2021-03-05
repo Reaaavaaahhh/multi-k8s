242061c69db7862335a9bcfe28fa1ed4ec95c96a
reaaavaaahhh <reaaavaaahhh@gmail.com>
2021-03-05 14:36:34 +0200

    changes to deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,20 @@ docker push reaaavaaahhh/multi-client:$SHA
 docker push reaaavaaahhh/multi-server:$SHA
 docker push reaaavaaahhh/multi-worker:$SHA
 
-kubectl apply -f .
+kubectl apply -f client-cluster-ip-service.yaml
+kubectl apply -f client-deployment.yaml
+kubectl apply -f database-persistent-volume-claim.yaml
+kubectl apply -f ingress-service.yaml
+kubectl apply -f nginx-ingress-controller.yaml
+kubectl apply -f postgres-cluster-ip-service.yaml
+kubectl apply -f postgres-deployment.yaml
+kubectl apply -f redis-cluster-ip-service.yaml
+kubectl apply -f redis-deployment.yaml
+kubectl apply -f server-cluster-ip-service.yaml
+kubectl apply -f server-deployment.yaml
+kubectl apply -f worker-deployment.yaml
+
+
 kubectl set image deployments/server-deployment server=reaaavaaahhh/multi-server:$SHA
 kubectl set image deployments/client/deployment client=reaaavaaahhh/multi-client:$SHA
 kubectl set image deployments/worker/deployment worker=reaaavaaahhh/multi-worker:$SHA
