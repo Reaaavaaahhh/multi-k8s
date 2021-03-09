8bae6ba33172764974fd99a8f43cf06c64179817
reaaavaaahhh <reaaavaaahhh@gmail.com>
2021-03-09 10:19:41 +0200

    Update to multiple files

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,6 @@ kubectl apply -f server-deployment.yaml
 kubectl apply -f worker-deployment.yaml
 
 
-#kubectl set image deployments/server-deployment server=reaaavaaahhh/multi-server:$SHA --ignore-not-found=true
-#kubectl set image deployments/client/deployment client=reaaavaaahhh/multi-client:$SHA --ignore-not-found=true
-#kubectl set image deployments/worker/deployment worker=reaaavaaahhh/multi-worker:$SHA --ignore-not-found=true
+kubectl set image deployments/server-deployment server=reaaavaaahhh/multi-server:$SHA
+kubectl set image deployments/client/deployment client=reaaavaaahhh/multi-client:$SHA
+kubectl set image deployments/worker/deployment worker=reaaavaaahhh/multi-worker:$SHA
